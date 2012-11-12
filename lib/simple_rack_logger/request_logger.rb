b39a38b2d448cd911dcfc17fe2b1143978809f32
Adriano Dadario <dev+dadario@vagas.com.br>
2012-11-12 16:52:36 -0200

    Adicionando filtro para evitar log de info indesejada

--- lib/simple_rack_logger/request_logger.rb
+++ lib/simple_rack_logger/request_logger.rb
@@ -4,9 +4,10 @@ module Rack
   class RequestLogger
     attr_reader :logger
 
-    def initialize(app, logger)
-      @app = app
-      @logger = logger
+    def initialize(app, logger, *filters)
+      @app     = app
+      @logger  = logger
+      @filters = filters
 
       def @logger.flush
         # Chamando o flush diretamente, pois está demorando para descarregar a memória
@@ -14,18 +15,23 @@ module Rack
         logdev = self.instance_variable_get(:@logdev)
         logdev.instance_variable_get(:@dev).flush if logdev
       end
-
     end
 
     def call(env)
-      logger.info(expose(Rack::Request.new(env)))
-      logger.flush
+      request = Rack::Request.new(env)
+      block = @filters.select { |filter| filter.respond_to?(:filter) && filter.send(:filter, request) }
+
+      if block.empty?
+        logger.info(expose(request))
+        logger.flush
+      end
+
       @app.call(env)
     end
 
     private
     def expose(request)
-      params = request.params.map{|key, value| "#{key}=#{value}"}
+      params = request.params.map { |key, value| "#{key}=#{value}" }
       "#{request.request_method}: #{request.url} for #{request.ip} with #{request.user_agent} [params: #{params.join('; ')}]"
     end
 
--- spec/simple_rack_logger/request_logger_spec.rb
+++ spec/simple_rack_logger/request_logger_spec.rb
@@ -3,16 +3,12 @@ require 'spec_helper'
 describe Rack::RequestLogger do
 
   let(:logger) { logger = double(Logger) }
-
-  subject {
-    app = mock(Struct.new(:none))
-    app.should_receive(:call)
-    Rack::RequestLogger.new(app, logger)
+  let(:app) {
+    mock(Struct.new(:none))
   }
 
-  it 'log request information from env' do
-
-    env = {
+  let(:env) do
+    {
       "GATEWAY_INTERFACE" => "CGI/1.1",
       "PATH_INFO"         => "/v1/crachas.xml",
       "QUERY_STRING"      => "",
@@ -37,10 +33,35 @@ describe Rack::RequestLogger do
       "rack.run_once"     => false,
       "rack.url_scheme"   => "http"
     }
+  end
 
-    logger.should_receive(:info).with('GET: http://localhost:9393/v1/crachas.xml for 127.0.0.1 with curl/7.22.0 (x86_64-pc-linux-gnu) libcurl/7.22.0 OpenSSL/1.0.1 zlib/1.2.3.4 libidn/1.23 librtmp/2.3 [params: ]')
+  context 'inicial state without filter' do
+    subject {
+      Rack::RequestLogger.new(app, logger)
+    }
 
-    subject.call(env)
+    it 'log request information from env' do
+      app.should_receive(:call)
+      logger.should_receive(:info).with('GET: http://localhost:9393/v1/crachas.xml for 127.0.0.1 with curl/7.22.0 (x86_64-pc-linux-gnu) libcurl/7.22.0 OpenSSL/1.0.1 zlib/1.2.3.4 libidn/1.23 librtmp/2.3 [params: ]')
+      subject.call(env)
+    end
+  end
+
+  context 'inicial state with filter' do
+    subject {
+      filter = Object.new
+      def filter.filter(request)
+        request.ip =~/127.0.0.1/
+      end
+      Rack::RequestLogger.new(app, logger, filter)
+    }
+
+    it 'dont log request information from env' do
+      app.should_receive(:call)
+      logger.should_not_receive(:info)
+      subject.call(env)
+    end
+
   end
 
 end
